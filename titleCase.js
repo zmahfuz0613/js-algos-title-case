a69fc52c636ada212ce7c1cce56d36f1456de8a4
mickmed <mick2090@gmail.com>
2020-02-25 21:02:05 -0500

    mods

--- titleCase.js
+++ titleCase.js
@@ -1,14 +1,10 @@
 //"capitilize the first letter of each word"
 
 const titleCase = str => {
-  let words = str.toLowerCase().split(" ");
-  for (let i = 0; i < words.length; i++) {
-    words[i] = words[i][0].toUpperCase() + words[i].slice(1);
-  }
-  return words.join(" ");
-};
-console.log(titleCase("I'm a little tea pot"));
+
+}
+console.log(titleCase("I'm a little tea pot"))
 
 module.exports = {
   titleCase
-};
+}
