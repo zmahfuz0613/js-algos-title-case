79c1f9d26ce756afe90845df8c3cd903f3b80a95
Misha Kessler <misha.kessler@generalassemb.ly>
2020-03-25 12:00:58 -0400

    Delete solution.js

--- solution.js
+++
@@ -1,8 +0,0 @@
-const titleCase = str => {
-    let words = str.toLowerCase().split(" ")
-    for (let i = 0; i < words.length; i++) {
-      words[i] = words[i][0].toUpperCase() + words[i].slice(1)
-    }
-    return words.join(" ")
-  }
-  console.log(titleCase("I'm a little tea pot"))
