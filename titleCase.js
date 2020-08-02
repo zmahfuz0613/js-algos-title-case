5678b00cdf80572b4cbde12f4b13b796e08a59ef
zmahfuz061318 <str053mgr@gmail.com>
2020-08-01 22:46:25 -0400

    Completed

--- titleCase.js
+++ titleCase.js
@@ -1,7 +1,9 @@
 // TITLE CASE
 
 const titleCase = str => {
-
+  return str.toLowerCase().split(' ').map(function(word) {
+    return word.startsWith('\'') ? (word.slice(0, 1) + word.charAt(1).toUpperCase() + word.slice(2)) : (word.charAt(0).toUpperCase() + word.slice(1));
+  }).join(' ');
 }
 
 console.log(titleCase("I'm a little tea pot"))
@@ -9,4 +11,3 @@ console.log(titleCase("I'm a little tea pot"))
 module.exports = {
   titleCase
 }
-// /...
